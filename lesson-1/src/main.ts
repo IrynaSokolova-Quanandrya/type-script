a09a81ee8575dc7e250076990473c6c3e4fdcd02
Iryna <irasokolova25@ukr.net>
2024-09-07 12:24:57 +0300

    Update main.ts

--- lesson-1/src/main.ts
+++ lesson-1/src/main.ts
@@ -15,6 +15,17 @@ console.log(name);
 const userId: string | number = 1;
 const playerId: string | number = 'khjhg5124scknchg';
 
+type UserObj = {
+    name: string,
+    lastName: string,
+    isOnline: boolean
+}
+
+const user: UserObj = {
+    name: 'Ira',
+    lastName: 'Lee',
+    isOnline: false
+}
 
 /**
  * СКЛАДНІ ТИПИ
@@ -25,6 +36,20 @@ const playerId: string | number = 'khjhg5124scknchg';
  * 2. type як опис об'єктів або коли є перелік сталих значень
  */
 
+const numbers: number[] = [1, 2, 3, 4];
+numbers.push(10)
+console.log(numbers);
+// const arr = [...numbers]
+// arr.push()
+
+// Tuple
+const coord: [number, number] = [31.55454, 54.15415]
+const entries: [string, boolean] = ['isOnline', false]
+const rgb: [number, number, number] = [255, 0, 255]
+
+
+
+
 /**
  * Кастомні типи з Type
  * - ідентифікатор з type composition 
@@ -33,3 +58,15 @@ const playerId: string | number = 'khjhg5124scknchg';
  * - tuple - [number, number]
  * - union - 'request' | 'success' | "error"
  */
+type Alert = 'request' | 'success' | "error"
+const alert: Alert = 'request'
+
+function calcNumb(numbers:number[]):number {
+    return numbers
+        .reduce((acc, number) => {
+            acc + number
+            return acc
+        }, 0)
+}
+
+calcNumb(numbers)
