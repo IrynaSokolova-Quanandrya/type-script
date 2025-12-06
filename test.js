c3d9d7c44df7f1ac456b9893668da2b22e6cc979
Iryna <irasokolova25@ukr.net>
2025-12-06 16:25:04 +0200

    add react-ts

--- test.js
+++
@@ -1,4 +0,0 @@
-function getSum(num1, num2) {
-    console.log(num1 + num2);
-}
-getSum("1", 10);
--- test.ts
+++
@@ -1,5 +0,0 @@
-function getSum(num1:number, num2:number) {
-    console.log(num1 + num2) 
-}
-
-getSum("1", 10)
